849a8f98a6fbd9d96b08adf53c64f5713bea3401
Josh Lukens <jlukens@botch.com>
2026-02-05 01:10:45 -0500

    form tweaks

--- src/components/layout/auth-loading-toast.tsx
+++ src/components/layout/auth-loading-toast.tsx
@@ -1,14 +1,18 @@
 "use client"
 
+import { useEffect } from "react"
 import { toast } from "sonner"
 
 export function WelcomeToast() {
-    const promise = () => new Promise((resolve) => setTimeout(() => resolve({ name: 'Sonner' }), 2000));
+    useEffect(() => {
+        const promise = () => new Promise((resolve) => setTimeout(() => resolve({ name: 'Sonner' }), 2000))
 
-    toast.promise(promise, {
-      loading: 'Authenticating...',
-      success: 'Welcome 👋 You are now logged in.',
-      error: 'Error',
-    });
+        toast.promise(promise, {
+            loading: 'Authenticating...',
+            success: 'Welcome 👋 You are now logged in.',
+            error: 'Error',
+        })
+    }, [])
+
     return null
 } 
