34de5be57414f1ffb5f67439483ca9390194202b
Josh Lukens <jlukens@botch.com>
2026-02-24 18:48:12 -0500

    add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Providers } from "./providers"
 import type { ReactNode } from "react"
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "@/styles/globals.css"
 
 export default function RootLayout({
@@ -16,6 +17,7 @@ export default function RootLayout({
             <body className="flex min-h-svh flex-col antialiased">
                 <Providers>{children}</Providers>
                 <Analytics />
+                <SpeedInsights />
             </body>
         </html>
     )
